9185474ef5b7357f55380540b1c4e4a7656e0217
Miro-02 <mirosalvmirchev2002@gmail.com>
2025-03-23 22:27:25 +0200

    Fixed env and gitignores

--- FlaskServer/pretrained.py
+++ FlaskServer/pretrained.py
@@ -108,4 +108,8 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    host = os.environ.get('APP_URL', 'http://flyaway-flask.localhost')
+    if host.startswith(('http://', 'https://')):
+        host = host.split('://', 1)[1]
+    port = int(os.environ.get('APP_PORT', 5000))
+    app.run(host=host, port=port, debug=False)
